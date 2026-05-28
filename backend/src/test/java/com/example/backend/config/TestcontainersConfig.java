eeadc85ec142ca7f0ee723494c72c3c9ca7e822c
Marko Niemi <marko.j.niemi@gmail.com>
2026-05-28 21:32:19 +0300

    Migrate TestcontainersConfig to new Container class; configure test users and OAuth2 client

--- backend/src/test/java/com/example/backend/config/TestcontainersConfig.java
+++ backend/src/test/java/com/example/backend/config/TestcontainersConfig.java
@@ -1,50 +1,32 @@
 package com.example.backend.config;
 
-import com.github.dockerjava.api.command.CreateContainerCmd;
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.HostConfig;
-import com.github.dockerjava.api.model.PortBinding;
-import com.github.dockerjava.api.model.Ports;
-import java.util.function.Consumer;
-import lombok.NonNull;
+import com.github.markoniemi.oauth2server.testcontainers.Client;
+import com.github.markoniemi.oauth2server.testcontainers.Container;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 @Slf4j
 public class TestcontainersConfig {
-  @Bean(destroyMethod = "")
-  @RestartScope
-  public GenericContainer<?> authorizationServerContainer() {
-    GenericContainer<?> authorizationServer =
-        new GenericContainer<>(DockerImageName.parse("ghcr.io/markoniemi/oauth2-server:latest"))
-            .withExposedPorts(9000)
-            .withLogConsumer(new Slf4jLogConsumer(log))
-            .withCreateContainerCmdModifier(getPortConfig())
-            .waitingFor(Wait.forHttp("/.well-known/openid-configuration").forStatusCode(200));
-    authorizationServer.start();
-    return authorizationServer;
+  private static Container sharedContainer;
+
+  @Bean(destroyMethod = "stop")
+  public Container oAuth2ServerContainer() {
+    if (sharedContainer == null) {
+      sharedContainer = new Container()
+          .withUser("testuser", "password", "USER")
+          .withUser("admin", "password", "ADMIN", "USER")
+          .withOAuth2Client(new Client("test-client", "test-secret")
+              .withRedirectUri("http://localhost:8080/callback")
+              .withRedirectUri("http://localhost:3000/callback")
+              .withScopes("openid", "profile", "email"));
+      sharedContainer.start();
+    }
+    return sharedContainer;
   }
 
-  @Bean
-  public CommandLineRunner ensureContainerStarted(
-      GenericContainer<?> authorizationServerContainer) {
-    return args ->
-        log.info("Auth server container started: {}", authorizationServerContainer.isRunning());
-  }
-
-  private static @NonNull Consumer<CreateContainerCmd> getPortConfig() {
-    return cmd ->
-        cmd.withHostConfig(
-            new HostConfig()
-                .withPortBindings(
-                    new PortBinding(Ports.Binding.bindPort(9000), new ExposedPort(9000))));
+  public static Container getSharedContainer() {
+    return sharedContainer;
   }
 }
