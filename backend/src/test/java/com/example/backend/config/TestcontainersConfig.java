0d22642b73ea7a4acb4db357e8ec6fe5803f3df7
Marko Niemi <marko.j.niemi@gmail.com>
2026-05-31 12:10:20 +0300

    Use auth-server with testcontainer.

--- backend/src/test/java/com/example/backend/config/TestcontainersConfig.java
+++ backend/src/test/java/com/example/backend/config/TestcontainersConfig.java
@@ -1,38 +1,29 @@
 package com.example.backend.config;
 
+import com.example.auth.testcontainers.OAuth2Container;
 import com.github.dockerjava.api.command.CreateContainerCmd;
 import com.github.dockerjava.api.model.ExposedPort;
 import com.github.dockerjava.api.model.HostConfig;
 import com.github.dockerjava.api.model.PortBinding;
 import com.github.dockerjava.api.model.Ports;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.function.Consumer;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.containers.BindMode;
-import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
 @Slf4j
 public class TestcontainersConfig {
 
   @Bean(destroyMethod = "")
-  public GenericContainer<?> authorizationServerContainer() {
-    Path configPath = Paths.get("backend/src/test/resources").toAbsolutePath();
-    GenericContainer<?> authorizationServer =
-        new GenericContainer<>(DockerImageName.parse("ghcr.io/markoniemi/oauth2-server:latest"))
-            .withExposedPorts(9000)
-            .withFileSystemBind(configPath.toString(), "/app/config", BindMode.READ_ONLY)
-            .withEnv("OAUTH2_CONFIG_PATH", "/app/config/oauth2-config.yaml")
+  public OAuth2Container authorizationServerContainer() {
+    OAuth2Container authorizationServer =
+        new OAuth2Container()
+            .withConfigFile("oauth2-config.yaml")
             .withLogConsumer(new Slf4jLogConsumer(log))
-            .withCreateContainerCmdModifier(getPortConfig())
-            .waitingFor(Wait.forHttp("/.well-known/openid-configuration").forStatusCode(200));
+            .withCreateContainerCmdModifier(getPortConfig());
     authorizationServer.start();
     return authorizationServer;
   }
